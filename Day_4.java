3e56489534c65f9c385d46e39402ba163ad3db37
kottegangaiah9-boop <kottegangaiah9@gmail.com>
2026-08-03 14:15:44 +0530

    Create Day_4.java

---
+++ Day_4.java
@@ -0,0 +1,73 @@
+...............................Hello  Square.....................................................
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+...............................Square pattern.....................................................
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+...............................inverted right triangle.....................................................
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+...............................right triangle.....................................................
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+...............................pyramid patern.....................................................
+public class Pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
